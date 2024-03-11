1795a05a443efb94d4127be8cca9ed4bea942af0
SUKHWINDERKAU <156840502+SUKHWINDERKAU@users.noreply.github.com>
2024-03-11 12:51:57 -0400

    Update red.php

--- red.php
+++ red.php
@@ -6,13 +6,13 @@
 		<meta name="author" content="The name of the document’s author to go here">
 		<meta name="description" content="A concise and accurate summary of the document content is to appear here">
 		<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
-		<link rel="stylesheet" href="css/normalize.css" />
-		<link rel="stylesheet" href="css/styles.css" />
+		<link rel="stylesheet" href="normalize.css" />
+		<link rel="stylesheet" href="styles.css" />
 	</head>
 	<body id="section2">
 		<!-- Page-level header -->
 		<header>
-			<img src="images/html-logo.svg" height="75" width="75" alt="HTML" />
+			<img src="html-logo.svg" height="75" width="75" alt="HTML" />
 			<h1>HTML, CSS, and JS Fundamentals</h1>
 			<h2>Building Blocks for Web Developers</h2>
 <?php
